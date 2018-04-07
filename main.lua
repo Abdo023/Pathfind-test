5e76542bf2e5c20476a28d6b46cf0836c9cf12f6
Abdou23 <abdelghafour.mohie@gmail.com>
2018-04-07 21:59:07 +0200

    Initial commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+display.setStatusBar( display.HiddenStatusBar )
+
+screen = require ( "Utils.screen")
+math2 = require ("Utils.math2")
+color = require ("Utils.color")
+
+
+local composer = require ("composer")
+composer.gotoScene( "Scene"  )
